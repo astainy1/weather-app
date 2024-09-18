573750028ef41d22b28c2400c3d3acced2581526
Astainy1 <astainyharris1@gmail.com>
2024-09-18 17:23:23 +0000

    updated layout and display temp in census

--- script.js
+++ script.js
@@ -3,63 +3,62 @@
 
 //Load DOM before script
 document.addEventListener('DOMContentLoaded', () => {
-    
-    //Get html element
-    // const searchbtn = document.getElementById('searchBtn').disabled = true;
+
+    //Get html elements
     const location = document.getElementById('location');
     const temperature = document.getElementById('temperature');
     const description = document.getElementById('description');
     const longitude = document.getElementById('longitude');
     const latitude = document.getElementById('latitude');
-    const icons = document.getElementById('icon')
+    const icons = document.getElementById('icon');
     const form = document.querySelector('form');
 
-    function weather(){
+    function weather() {
 
-        // const img = 'http://openweathermap.org/img/w/';
-        // const png = '.png';
         //Store input value
         let userInput = document.querySelector('#searchInput').value;
-        
-        //store api key into a variable
+
+        //Store API key into a variable
         const apiKEY = '5cb2d25bcf3179ae6823dff05d61557b';
-        
+
         //Condition for input field
-        if(userInput != ''){
+        if (userInput != '') {
 
-        document.getElementById('message').innerHTML = 'Location';
-        //Fetch api
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKEY)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data){
+            document.getElementById('message').innerHTML = 'Location';
 
-            location.innerHTML = data.name;
-            temperature.innerHTML = data.main.temp;
-            description.innerHTML = data.weather[0].description;
-            longitude.innerHTML = data.coord.lon;
-            latitude.innerHTML = data.coord.lat;
-          
-            const iconCode = data.weather[0].icon;
-            icons.src = `http://openweathermap.org/img/w/${iconCode}.png`
-            console.log(iconCode)
-            // icons.innerHTML = 
-            // console.log(data)
-        })
-        .catch(function() {
-            console.log('Error')
-        })}else{
-            document.getElementById('message').innerHTML = 'Invalid input'
+            //Fetch API with 'units=metric' for Celsius
+            fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKEY + '&units=metric')
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+
+                    location.innerHTML = data.name;
+
+                    // Display temperature in Celsius with °C symbol
+                    temperature.innerHTML = `${data.main.temp}°C`;
+
+                    description.innerHTML = data.weather[0].description;
+                    longitude.innerHTML = data.coord.lon;
+                    latitude.innerHTML = data.coord.lat;
+
+                    const iconCode = data.weather[0].icon;
+                    icons.src = `http://openweathermap.org/img/w/${iconCode}.png`;
+                    console.log(iconCode);
+                })
+                .catch(function () {
+                    console.log('Error');
+                });
+        } else {
+            document.getElementById('message').innerHTML = 'Invalid input';
         }
     }
 
-    //event listener for form submission
+    //Event listener for form submission
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        //invoking weather function upon submission
+        //Invoking weather function upon submission
         weather();
-        
-    })
-})
+    });
+});
